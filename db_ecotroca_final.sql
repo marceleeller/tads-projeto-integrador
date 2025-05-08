b20195a1b95bcd7cbb440dfaaaf57f116974abc7
arleteLeite <arlete.leite@gmail.com>
2025-05-08 05:55:32 -0300

    Atualização do banco: exclusão das tabelas Endereco_Produto e Avaliacao, e ajuste das chaves estrangeiras

--- db_ecotroca_final.sql
+++ db_ecotroca_final.sql
@@ -17,220 +17,220 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema ecotroca
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ecotroca` DEFAULT CHARACTER SET utf8 ;
-USE `ecotroca` ;
+CREATE SCHEMA IF NOT EXISTS ecotroca DEFAULT CHARACTER SET utf8 ;
+USE ecotroca ;
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`USUARIO`
+-- Table ecotroca.USUARIO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`USUARIO` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `nome_usuario` VARCHAR(150) NOT NULL,
-  `cpf` VARCHAR(14) NULL,
-  `telefone` VARCHAR(14) NOT NULL,
-  `email` VARCHAR(150) NOT NULL,
-  `senha` VARCHAR(250) NOT NULL,
-  `data_cadastro` DATETIME NOT NULL,
-  `data_nascimento` DATE NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
-  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC))
+CREATE TABLE IF NOT EXISTS ecotroca.USUARIO (
+  id_usuario INT NOT NULL AUTO_INCREMENT,
+  nome_usuario VARCHAR(150) NOT NULL,
+  cpf VARCHAR(14) NULL,
+  telefone VARCHAR(14) NOT NULL,
+  email VARCHAR(150) NOT NULL,
+  senha VARCHAR(250) NOT NULL,
+  data_cadastro DATETIME NOT NULL,
+  data_nascimento DATE NOT NULL,
+  PRIMARY KEY (id_usuario),
+  UNIQUE INDEX email_UNIQUE (email ASC),
+  UNIQUE INDEX cpf_UNIQUE (cpf ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`ENDERECO_USUARIO`
+-- Table ecotroca.ENDERECO_USUARIO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`ENDERECO_USUARIO` (
-  `id_endereco` INT NOT NULL AUTO_INCREMENT,
-  `cep` VARCHAR(9) NOT NULL,
-  `bairro` VARCHAR(80) NOT NULL,
-  `rua` VARCHAR(80) NOT NULL,
-  `numero` VARCHAR(10) NOT NULL,
-  `complemento` VARCHAR(150) NULL,
-  `cidade` VARCHAR(80) NOT NULL,
-  `estado` VARCHAR(50) NOT NULL,
-  `id_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_endereco`),
-  INDEX `fk_ENDERECO_USUARIO1_idx` (`id_usuario` ASC),
-  CONSTRAINT `fk_ENDERECO_USUARIO1`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `ecotroca`.`USUARIO` (`id_usuario`)
+CREATE TABLE IF NOT EXISTS ecotroca.ENDERECO_USUARIO (
+  id_endereco INT NOT NULL AUTO_INCREMENT,
+  cep VARCHAR(9) NOT NULL,
+  bairro VARCHAR(80) NOT NULL,
+  rua VARCHAR(80) NOT NULL,
+  numero VARCHAR(10) NOT NULL,
+  complemento VARCHAR(150) NULL,
+  cidade VARCHAR(80) NOT NULL,
+  estado VARCHAR(50) NOT NULL,
+  id_usuario INT NOT NULL,
+  PRIMARY KEY (id_endereco),
+  INDEX fk_ENDERECO_USUARIO1_idx (id_usuario ASC),
+  CONSTRAINT fk_ENDERECO_USUARIO1
+    FOREIGN KEY (id_usuario)
+    REFERENCES ecotroca.USUARIO (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`ENDERECO_PRODUTO`
+-- Table ecotroca.ENDERECO_PRODUTO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`ENDERECO_PRODUTO` (
-  `id_endereco_produto` INT NOT NULL AUTO_INCREMENT,
-  `cep` VARCHAR(9) NOT NULL,
-  `bairro` VARCHAR(80) NOT NULL,
-  `rua` VARCHAR(80) NOT NULL,
-  `numero` VARCHAR(10) NOT NULL,
-  `complemento` VARCHAR(150) NULL,
-  `cidade` VARCHAR(80) NOT NULL,
-  `estado` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_endereco_produto`))
+CREATE TABLE IF NOT EXISTS ecotroca.ENDERECO_PRODUTO (
+  id_endereco_produto INT NOT NULL AUTO_INCREMENT,
+  cep VARCHAR(9) NOT NULL,
+  bairro VARCHAR(80) NOT NULL,
+  rua VARCHAR(80) NOT NULL,
+  numero VARCHAR(10) NOT NULL,
+  complemento VARCHAR(150) NULL,
+  cidade VARCHAR(80) NOT NULL,
+  estado VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id_endereco_produto))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`PRODUTO`
+-- Table ecotroca.PRODUTO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`PRODUTO` (
-  `id_produto` INT NOT NULL AUTO_INCREMENT,
-  `nome_produto` VARCHAR(80) NOT NULL,
-  `descricao` VARCHAR(200) NOT NULL,
-  `status` ENUM('NOVO', 'USADO') NOT NULL,
-  `data_cadastro` DATE NOT NULL,
-  `quantidade` INT NOT NULL,
-  `valor` DECIMAL(10,2) NULL,
-  `id_usuario` INT NOT NULL,
-  `id_endereco_produto` INT NOT NULL,
-  PRIMARY KEY (`id_produto`),
-  INDEX `fk_PRODUTO_USUARIO_idx` (`id_usuario` ASC),
-  INDEX `fk_PRODUTO_ENDERECO_PRODUTO1_idx` (`id_endereco_produto` ASC),
-  CONSTRAINT `fk_PRODUTO_USUARIO`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `ecotroca`.`USUARIO` (`id_usuario`)
+CREATE TABLE IF NOT EXISTS ecotroca.PRODUTO (
+  id_produto INT NOT NULL AUTO_INCREMENT,
+  nome_produto VARCHAR(80) NOT NULL,
+  descricao VARCHAR(200) NOT NULL,
+  status ENUM('NOVO', 'USADO') NOT NULL,
+  data_cadastro DATE NOT NULL,
+  quantidade INT NOT NULL,
+  valor DECIMAL(10,2) NULL,
+  id_usuario INT NOT NULL,
+  id_endereco_produto INT NOT NULL,
+  PRIMARY KEY (id_produto),
+  INDEX fk_PRODUTO_USUARIO_idx (id_usuario ASC),
+  INDEX fk_PRODUTO_ENDERECO_PRODUTO1_idx (id_endereco_produto ASC),
+  CONSTRAINT fk_PRODUTO_USUARIO
+    FOREIGN KEY (id_usuario)
+    REFERENCES ecotroca.USUARIO (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_PRODUTO_ENDERECO_PRODUTO1`
-    FOREIGN KEY (`id_endereco_produto`)
-    REFERENCES `ecotroca`.`ENDERECO_PRODUTO` (`id_endereco_produto`)
+  CONSTRAINT fk_PRODUTO_ENDERECO_PRODUTO1
+    FOREIGN KEY (id_endereco_produto)
+    REFERENCES ecotroca.ENDERECO_PRODUTO (id_endereco_produto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`CATEGORIA`
+-- Table ecotroca.CATEGORIA
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`CATEGORIA` (
-  `id_categoria` INT NOT NULL AUTO_INCREMENT,
-  `nome_categoria` VARCHAR(20) NOT NULL,
-  `descricao` VARCHAR(80) NOT NULL,
-  PRIMARY KEY (`id_categoria`))
+CREATE TABLE IF NOT EXISTS ecotroca.CATEGORIA (
+  id_categoria INT NOT NULL AUTO_INCREMENT,
+  nome_categoria VARCHAR(20) NOT NULL,
+  descricao VARCHAR(80) NOT NULL,
+  PRIMARY KEY (id_categoria))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`MENSAGEM`
+-- Table ecotroca.MENSAGEM
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`MENSAGEM` (
-  `id_mensagem` INT NOT NULL AUTO_INCREMENT,
-  `conteudo_mensagem` VARCHAR(100) NOT NULL,
-  `data_envio` DATE NOT NULL,
-  `id_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_mensagem`),
-  INDEX `fk_MENSAGEM_USUARIO1_idx` (`id_usuario` ASC),
-  CONSTRAINT `fk_MENSAGEM_USUARIO1`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `ecotroca`.`USUARIO` (`id_usuario`)
+CREATE TABLE IF NOT EXISTS ecotroca.MENSAGEM (
+  id_mensagem INT NOT NULL AUTO_INCREMENT,
+  conteudo_mensagem VARCHAR(100) NOT NULL,
+  data_envio DATE NOT NULL,
+  id_usuario INT NOT NULL,
+  PRIMARY KEY (id_mensagem),
+  INDEX fk_MENSAGEM_USUARIO1_idx (id_usuario ASC),
+  CONSTRAINT fk_MENSAGEM_USUARIO1
+    FOREIGN KEY (id_usuario)
+    REFERENCES ecotroca.USUARIO (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`AVALIACAO`
+-- Table ecotroca.AVALIACAO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`AVALIACAO` (
-  `id_avaliacao` INT NOT NULL AUTO_INCREMENT,
-  `conteudo_avaliacao` VARCHAR(200) NULL,
-  `data_avaliacao` DATE NOT NULL,
-  `nota_avaliacao` ENUM('1', '2', '3', '4', '5') NOT NULL,
-  `id_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_avaliacao`),
-  INDEX `fk_AVALIACAO_USUARIO1_idx` (`id_usuario` ASC),
-  CONSTRAINT `fk_AVALIACAO_USUARIO1`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `ecotroca`.`USUARIO` (`id_usuario`)
+CREATE TABLE IF NOT EXISTS ecotroca.AVALIACAO (
+  id_avaliacao INT NOT NULL AUTO_INCREMENT,
+  conteudo_avaliacao VARCHAR(200) NULL,
+  data_avaliacao DATE NOT NULL,
+  nota_avaliacao ENUM('1', '2', '3', '4', '5') NOT NULL,
+  id_usuario INT NOT NULL,
+  PRIMARY KEY (id_avaliacao),
+  INDEX fk_AVALIACAO_USUARIO1_idx (id_usuario ASC),
+  CONSTRAINT fk_AVALIACAO_USUARIO1
+    FOREIGN KEY (id_usuario)
+    REFERENCES ecotroca.USUARIO (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`TRANSACAO`
+-- Table ecotroca.TRANSACAO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`TRANSACAO` (
-  `id_transacao` INT NOT NULL AUTO_INCREMENT,
-  `data_transacao` DATE NOT NULL,
-  `id_avaliacao` INT NOT NULL,
-  PRIMARY KEY (`id_transacao`),
-  INDEX `fk_TRANSACAO_AVALIACAO1_idx` (`id_avaliacao` ASC),
-  CONSTRAINT `fk_TRANSACAO_AVALIACAO1`
-    FOREIGN KEY (`id_avaliacao`)
-    REFERENCES `ecotroca`.`AVALIACAO` (`id_avaliacao`)
+CREATE TABLE IF NOT EXISTS ecotroca.TRANSACAO (
+  id_transacao INT NOT NULL AUTO_INCREMENT,
+  data_transacao DATE NOT NULL,
+  id_avaliacao INT NOT NULL,
+  PRIMARY KEY (id_transacao),
+  INDEX fk_TRANSACAO_AVALIACAO1_idx (id_avaliacao ASC),
+  CONSTRAINT fk_TRANSACAO_AVALIACAO1
+    FOREIGN KEY (id_avaliacao)
+    REFERENCES ecotroca.AVALIACAO (id_avaliacao)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`SOLICITACAO`
+-- Table ecotroca.SOLICITACAO
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`SOLICITACAO` (
-  `id_solicitacao` INT NOT NULL AUTO_INCREMENT,
-  `status` ENUM('PENDENTE', 'APROVADA', 'RECUSADA') NOT NULL,
-  `data_solicitacao` DATE NOT NULL,
-  `id_usuario` INT NOT NULL,
-  `id_transacao` INT NOT NULL,
-  PRIMARY KEY (`id_solicitacao`),
-  INDEX `fk_SOLICITACAO_USUARIO1_idx` (`id_usuario` ASC),
-  INDEX `fk_SOLICITACAO_TRANSACAO1_idx` (`id_transacao` ASC),
-  CONSTRAINT `fk_SOLICITACAO_USUARIO1`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `ecotroca`.`USUARIO` (`id_usuario`)
+CREATE TABLE IF NOT EXISTS ecotroca.SOLICITACAO (
+  id_solicitacao INT NOT NULL AUTO_INCREMENT,
+  status ENUM('PENDENTE', 'APROVADA', 'RECUSADA') NOT NULL,
+  data_solicitacao DATE NOT NULL,
+  id_usuario INT NOT NULL,
+  id_transacao INT NOT NULL,
+  PRIMARY KEY (id_solicitacao),
+  INDEX fk_SOLICITACAO_USUARIO1_idx (id_usuario ASC),
+  INDEX fk_SOLICITACAO_TRANSACAO1_idx (id_transacao ASC),
+  CONSTRAINT fk_SOLICITACAO_USUARIO1
+    FOREIGN KEY (id_usuario)
+    REFERENCES ecotroca.USUARIO (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_SOLICITACAO_TRANSACAO1`
-    FOREIGN KEY (`id_transacao`)
-    REFERENCES `ecotroca`.`TRANSACAO` (`id_transacao`)
+  CONSTRAINT fk_SOLICITACAO_TRANSACAO1
+    FOREIGN KEY (id_transacao)
+    REFERENCES ecotroca.TRANSACAO (id_transacao)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`IMAGEM`
+-- Table ecotroca.IMAGEM
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`IMAGEM` (
-  `id_imagem` INT NOT NULL AUTO_INCREMENT,
-  `url_imagem` VARCHAR(200) NOT NULL,
-  `descricao_imagem` VARCHAR(100) NULL,
-  `id_produto` INT NOT NULL,
-  PRIMARY KEY (`id_imagem`),
-  INDEX `fk_IMAGEM_PRODUTO1_idx` (`id_produto` ASC),
-  CONSTRAINT `fk_IMAGEM_PRODUTO1`
-    FOREIGN KEY (`id_produto`)
-    REFERENCES `ecotroca`.`PRODUTO` (`id_produto`)
+CREATE TABLE IF NOT EXISTS ecotroca.IMAGEM (
+  id_imagem INT NOT NULL AUTO_INCREMENT,
+  url_imagem VARCHAR(200) NOT NULL,
+  descricao_imagem VARCHAR(100) NULL,
+  id_produto INT NOT NULL,
+  PRIMARY KEY (id_imagem),
+  INDEX fk_IMAGEM_PRODUTO1_idx (id_produto ASC),
+  CONSTRAINT fk_IMAGEM_PRODUTO1
+    FOREIGN KEY (id_produto)
+    REFERENCES ecotroca.PRODUTO (id_produto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecotroca`.`PRODUTO_CATEGORIA`
+-- Table ecotroca.PRODUTO_CATEGORIA
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecotroca`.`PRODUTO_CATEGORIA` (
-  `id_produto` INT NOT NULL,
-  `id_categoria` INT NOT NULL,
-  PRIMARY KEY (`id_produto`, `id_categoria`),
-  INDEX `fk_PRODUTO_CATEGORIA_CATEGORIA1_idx` (`id_categoria` ASC),
-  INDEX `fk_PRODUTO_CATEGORIA_PRODUTO1_idx` (`id_produto` ASC),
-  CONSTRAINT `fk_PRODUTO_CATEGORIA_PRODUTO1`
-    FOREIGN KEY (`id_produto`)
-    REFERENCES `ecotroca`.`PRODUTO` (`id_produto`)
+CREATE TABLE IF NOT EXISTS ecotroca.PRODUTO_CATEGORIA (
+  id_produto INT NOT NULL,
+  id_categoria INT NOT NULL,
+  PRIMARY KEY (id_produto, id_categoria),
+  INDEX fk_PRODUTO_CATEGORIA_CATEGORIA1_idx (id_categoria ASC),
+  INDEX fk_PRODUTO_CATEGORIA_PRODUTO1_idx (id_produto ASC),
+  CONSTRAINT fk_PRODUTO_CATEGORIA_PRODUTO1
+    FOREIGN KEY (id_produto)
+    REFERENCES ecotroca.PRODUTO (id_produto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_PRODUTO_CATEGORIA_CATEGORIA1`
-    FOREIGN KEY (`id_categoria`)
-    REFERENCES `ecotroca`.`CATEGORIA` (`id_categoria`)
+  CONSTRAINT fk_PRODUTO_CATEGORIA_CATEGORIA1
+    FOREIGN KEY (id_categoria)
+    REFERENCES ecotroca.CATEGORIA (id_categoria)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -242,3 +242,22 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 -- reativa a verificação da unicidade
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- alterando a tabela produto, excluindo a chave estrangeira para poder apagar a tabela end do produto
+ALTER TABLE PRODUTO DROP FOREIGN KEY fk_PRODUTO_ENDERECO_PRODUTO1;
+
+-- excluindo a tabela endereco_produto
+drop table endereco_produto;
+
+-- alterando a tabela transacao, excluindo a chave estrangeira para poder apagar a tabela avaliacao
+ALTER TABLE TRANSACAO DROP FOREIGN KEY fk_TRANSACAO_AVALIACAO1;
+
+-- excluindo a tabela avaliacao
+drop table avaliacao;
+
+-- excluindo as colunas das chaves estrangeiras nas tabelas produto e transacao
+DROP TABLE PRODUTO;
+DROP TABLE AVALIACAO;
+
+show tables;
+
